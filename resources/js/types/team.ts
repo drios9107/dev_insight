bc3c0ec8b582af9cc56c0b906f198be404758c57
David Rios <drios9107@gmail.com>
2026-08-03 13:08:17 -0400

    created teams interface

---
+++ resources/js/types/team.ts
@@ -0,0 +1,10 @@
+export interface ITeam {
+    id: string;
+    name: string;
+    description: string;
+    owner: string;
+    avatar_url: string;
+    is_active: boolean;
+    created_at: string;
+    updated_at: string;
+} 
