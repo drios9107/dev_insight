30eedf48b68b7b8f199db0bea0ba632ed6e4ab0a
David Rios <drios9107@gmail.com>
2026-08-03 13:08:05 -0400

    created initial menu

--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -1,5 +1,5 @@
 import { Link } from '@inertiajs/react';
-import { BookOpen, FolderGit2, LayoutGrid } from 'lucide-react';
+import { BookOpen, Bug, CheckSquare, Cloud, DiamondPercent, FolderGit2, GitCommit, Group, LayoutDashboard, LayoutGrid, ListCheck, ListStart, Notebook, ReceiptPoundSterling, Shield, Users2, Workflow } from 'lucide-react';
 import AppLogo from '@/components/app-logo';
 import { NavFooter } from '@/components/nav-footer';
 import { NavMain } from '@/components/nav-main';
@@ -15,12 +15,82 @@ import {
 } from '@/components/ui/sidebar';
 import { dashboard } from '@/routes';
 import type { NavItem } from '@/types';
+import user from '@/routes/user';
+import team from '@/routes/team';
+import comment from '@/routes/comment';
+import commit from '@/routes/commit';
+import githubIssue from '@/routes/github-issue';
+import githubRepository from '@/routes/github-repository';
+import metric from '@/routes/metric';
+import notification from '@/routes/notification';
+import project from '@/routes/project';
+import pullRequest from '@/routes/pull-request';
+import pullRequestReview from '@/routes/pull-request-review';
+import sprint from '@/routes/sprint';
+import task from '@/routes/task';
+import role from '@/routes/role';
 
 const mainNavItems: NavItem[] = [
     {
         title: 'Dashboard',
         href: dashboard(),
         icon: LayoutGrid,
+    }, {
+        title: 'Comment',
+        href: comment.index(),
+        icon: Notebook,
+    }, {
+        title: 'Commit',
+        href: commit.index(),
+        icon: GitCommit,
+    }, {
+        title: 'Issues',
+        href: githubIssue.index(),
+        icon: Bug,
+    }, {
+        title: 'Repositories',
+        href: githubRepository.index(),
+        icon: FolderGit2,
+    }, {
+        title: 'Metrics',
+        href: metric.index(),
+        icon: LayoutDashboard,
+    }, {
+        title: 'Notifications',
+        href: notification.index(),
+        icon: Cloud,
+    }, {
+        title: 'Projects',
+        href: project.index(),
+        icon: Workflow,
+    }, {
+        title: 'PRs',
+        href: pullRequest.index(),
+        icon: DiamondPercent,
+    }, {
+        title: 'PR Reviewers',
+        href: pullRequestReview.index(),
+        icon: CheckSquare,
+    }, {
+        title: 'Sprints',
+        href: sprint.index(),
+        icon: ListStart,
+    }, {
+        title: 'Tasks',
+        href: task.index(),
+        icon: ListCheck,
+    }, {
+        title: 'Teams',
+        href: team.index(),
+        icon: Group,
+    }, {
+        title: 'Roles',
+        href: role.index(),
+        icon: Shield,
+    }, {
+        title: 'Users',
+        href: user.index(),
+        icon: Users2,
     },
 ];
 
