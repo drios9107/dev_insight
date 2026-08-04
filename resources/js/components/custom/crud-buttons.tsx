6d1e516db6240752887513dc31ad2480dfa190db
David Rios <drios9107@gmail.com>
2026-08-04 12:57:59 -0400

    fixed crudbuttons ts annotations

--- resources/js/components/custom/crud-buttons.tsx
+++ resources/js/components/custom/crud-buttons.tsx
@@ -1,12 +1,17 @@
 import { List, Pencil, Trash } from "lucide-react"
 import { Button } from "../ui/button"
 
-// @todo: fix ts annotation {onDetails:something}
-const CrudButtons = (props: any) => {
+interface ICrudButtonsProps {
+    onDetails?: () => void;
+    onEdit?: () => void;
+    onDelete?: () => void;
+}
+
+const CrudButtons = ({ onDetails, onEdit, onDelete }: ICrudButtonsProps) => {
     return <div className="flex gap-2 justify-center flex-wrap ">
-        {props.onDetails && <Button className="bg-blue-600" onClick={props.onDetails}><List /></Button>}
-        {props.onEdit && <Button className="bg-yellow-600" onClick={props.onEdit}><Pencil /></Button>}
-        {props.onDelete && <Button className="bg-red-600" onClick={props.onDelete}><Trash /></Button>}
+        {onDetails && <Button className="bg-blue-600" onClick={onDetails}><List /></Button>}
+        {onEdit && <Button className="bg-yellow-600" onClick={onEdit}><Pencil /></Button>}
+        {onDelete && <Button className="bg-red-600" onClick={onDelete}><Trash /></Button>}
     </div>
 }
 
