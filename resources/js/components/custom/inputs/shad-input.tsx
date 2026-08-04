06fc592f618a0075b102f8bc47b62d1e7cd5f9db
David Rios <drios9107@gmail.com>
2026-08-04 13:42:54 -0400

    added error message to shad-input component

--- resources/js/components/custom/inputs/shad-input.tsx
+++ resources/js/components/custom/inputs/shad-input.tsx
@@ -1,11 +1,13 @@
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import { IHtmlWithLabel } from "@/types"
 
-const ShadInput = ({ label, id, name, value, onChange, ...props }: { label: string, id?: string, name: string, value: string, onChange: (e: React.ChangeEvent<HTMLInputElement>) => void }) => {
+const ShadInput = ({ label, id, name, value, onChange, errors, ...props }: IHtmlWithLabel) => {
     return (
         <div className="flex flex-col gap-2">
             <Label htmlFor={id ?? name}>{label}</Label>
             <Input id={id ?? name} name={name} value={value} onChange={onChange} {...props} />
+            {name && errors?.[name] && <span className="text-red-600 text-sm px-1" style={{ marginTop: -8 }}>{errors[name]}</span>}
         </div>
     )
 }
