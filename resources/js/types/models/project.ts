ebef40e3dd645392f1069da8dd5ccaac61901083
David Rios <drios9107@gmail.com>
2026-08-05 16:33:29 -0400

    created project model types file

---
+++ resources/js/types/models/project.ts
@@ -0,0 +1,19 @@
+import { IUser } from "../user";
+import { ITeam } from "./team";
+
+export interface IProject {
+    id: number;
+    description: string;
+    name: string;
+    team: ITeam;
+    owner: IUser;
+    github_repository: number;
+    status: TProjectStatus;
+    start_date: string;
+    end_date: string;
+    color: string;
+    created_at: string;
+    updated_at: string;
+}
+
+export type TProjectStatus = 'planning' | 'active' | 'paused' | 'completed' | 'archived'
