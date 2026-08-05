f5b37de67c82e5b9bfcffdbaa93a54a59d94cd77
David Rios <drios9107@gmail.com>
2026-08-05 14:35:57 -0400

    fixed loader

--- resources/js/components/custom/loader.tsx
+++ resources/js/components/custom/loader.tsx
@@ -1,5 +1,9 @@
 import { Loader2 } from "lucide-react"
 
 export function Loader() {
-    return <Loader2 className="absolute self-center h-6 w-6 animate-spin text-blue-600" />
+    return (
+        <div className="absolute flex items-center justify-center min-h-screen w-full">
+            <Loader2 className="h-8 w-8 animate-spin text-blue-600" />
+        </div>
+    )
 }
