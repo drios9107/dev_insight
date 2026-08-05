f6fe8c153c855436751e166cbe315630661a79be
David Rios <drios9107@gmail.com>
2026-08-05 16:34:34 -0400

    defined project resource

--- app/Http/Resources/ProjectResource.php
+++ app/Http/Resources/ProjectResource.php
@@ -14,6 +14,19 @@ class ProjectResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'description' => $this->description,
+            'team' => new TeamResource($this->team),
+            'owner' => new UserResource($this->owner),
+            'github_repository' => new GithubRepositoryResource($this->github_repository),
+            'status' => $this->status,
+            'color' => $this->color,
+            'start_date' => date_format($this->start_date, 'Y-m-d'),
+            'end_date' => date_format($this->end_date, 'Y-m-d'),
+            'created_at' => date_format($this->created_at, 'Y-m-d'),
+            'updated_at' => date_format($this->updated_at, 'Y-m-d'),
+        ];
     }
 }
