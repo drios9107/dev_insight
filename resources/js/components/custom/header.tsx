83d86c8b43206c671194864551a3a11c22a21860
David Rios <drios9107@gmail.com>
2026-08-08 17:27:09 -0400

    added custom header component

---
+++ resources/js/components/custom/header.tsx
@@ -0,0 +1,16 @@
+import { Plus } from "lucide-react"
+import { Button } from "../ui/button"
+
+interface IHeader {
+    title: string,
+    onClick: () => void
+}
+
+const Header = ({ title = '', onClick }: IHeader) => {
+    return <div className="flex justify-between items-center">
+        <h1 className="p-6">{title}</h1>
+        <Button className="bg-green-600 mx-6" onClick={onClick}><Plus /></Button>
+    </div>
+}
+
+export default Header
