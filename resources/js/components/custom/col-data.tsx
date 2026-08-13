fb4c7cf85eb588714e6267e2673d999cd6710a62
David Rios <drios9107@gmail.com>
2026-08-13 16:26:14 -0400

    added coldata styles

--- resources/js/components/custom/col-data.tsx
+++ resources/js/components/custom/col-data.tsx
@@ -3,8 +3,10 @@ const ColData = ({ title, value }: {
     title: string,
     value?: string | number;
 }) => {
+    if (!value) return null
+
     return <div className="flex flex-col gap-1 justify-center items-center">
-        <span className="font-semibold">
+        <span className="font-semibold" style={{ userSelect: 'none' }}>
             {title}:
         </span>
         <span>
