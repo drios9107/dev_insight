d426b8baa9a906e5f8f1397654244bed61fc996d
David Rios <drios9107@gmail.com>
2026-09-09 09:57:56 -0400

    added child in header component

--- resources/js/components/custom/header.tsx
+++ resources/js/components/custom/header.tsx
@@ -3,14 +3,18 @@ import { Button } from "../ui/button"
 
 interface IHeader {
     title: string,
-    onClick?: () => void
+    onClick?: () => void,
+    children?: React.ReactNode
 }
 
-const Header = ({ title = '', onClick }: IHeader) => {
-    return <div className="flex justify-between items-center">
-        <h1 className="p-6">{title}</h1>
-        {onClick && <Button variant="outline" className="text-green-600 mx-6" onClick={onClick}><Plus className="w-4 h-4" /></Button>}
-    </div>
+const Header = ({ title = '', onClick, children }: IHeader) => {
+    return (
+        <div className="flex justify-between items-center">
+            <h1 className="p-6">{title}</h1>
+            {onClick && <Button variant="outline" className="text-green-600 mx-6" onClick={onClick}><Plus className="w-4 h-4" /></Button>}
+            {children}
+        </div>
+    )
 }
 
 export default Header
