7e2ee9b4298851d3e6fca45b0797da6b6dfe6272
David Rios <drios9107@gmail.com>
2026-09-16 04:40:28 -0400

    created ExtractsFilters trait

---
+++ app/Concerns/ExtractsFilters.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Concerns;
+
+use Illuminate\Http\Request;
+
+trait ExtractsFilters
+{
+    protected function extractFilters(Request $request, array $extraKeys = []): array
+    {
+        $commonKeys = ['search', 'sort', 'direction', 'per_page'];
+        $keys = array_merge($commonKeys, $extraKeys);
+
+        $filters = [];
+        foreach ($keys as $key) {
+            $value = $request->input($key);
+
+            if (!is_null($value) && $value !== '') {
+                $filters[$key] = $value;
+            }
+        }
+
+        return $filters;
+    }
+}
